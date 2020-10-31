0d37cd6d4fd6d62832bda614c4e81b5b829557bd
troylochner <troy.lochner@gmail.com>
2020-10-31 18:50:27 -0400

    Major code refactoring to simplify my workflow

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 let mysql = require("mysql");
 let inquirer = require("inquirer");
+const cTable = require("console.table");
+
+//MAKING SOME HOLDER VALUES FOR ARRAYS
+let deptArray = [];
+let roleArray = [];
+let employeeArray = [];
+//let managerArr = [];
+
+
 
 let connection = mysql.createConnection({
     host: "localhost",
@@ -11,179 +20,102 @@ let connection = mysql.createConnection({
 
 connection.connect(function (err) {
     if (err) throw err;
-    console.log("connected as id \n" +
-    connection.threadId);
-    //connection.end();
+    console.log("Welcome to the Employee Tracking Tool")
+    startPage();
 });
 
-let x = []; 
 
 console.log("Welcome to the employee tracker database !\n\n")
 
-const startPage = () =>{
-    return inquirer
-    .prompt([{
-        type:"list",
-        name:"action",
-        message:"Select a Database Section:",
-        choices:["Employees","Roles","Departments","Quit"],    
-    }])
-    .then ((data)=> {
-        switch (data.action) {
-            case "Employees":
-            console.log("startPage -> case:", data.action);
-            employeeActionsMenu();
-            break;
-            case "Roles":
-            console.log("startPage -> case:", data.action);
-            roleActionsMenu();
-            break;
-            case "Departments":
-            console.log("startPage -> case:", data.action);
-            departmentActionsMenu();
-            break;
-            case "Quit":
-            console.log("startPage -> case:", data.action); 
-            console.log(x)
-            connection.end();
-        }
-    });
-};
+const homeMenu = [
+    {
+      type: "list",
+      name: "action",
+      message: "What would you like to do?",
+      choices: [
+        "View All Employees",
+        "Add Employee",
+        "Update An Employee Role",
+        "View All Roles",
+        "Add Role",
+        "View All Departments",
+        "Add Department",
+        "View All Employees By Role",
+        "View All Employees By Department",     
+        "Exit",
+      ],
+    },
+  ];
 
-const employeeActionsMenu = () =>{
-    return inquirer
-    .prompt([{
-        type:"list",
-        name:"action",
-        message:"What would you like to do?",
-        choices:["Show All","Find","Add New","Go Back"], 
-    }])
-    .then((data)=> {
-        switch (data.action) {
-            case "Show All":
-            console.log("startPage -> case:", data.action);
-            showEmployees();
-            break;
-            case "Find":
-            console.log("startPage -> case:", data.action);
-            //showRoles();
-            break;
-            case "Add New":
-            console.log("startPage -> case:", data.action);
-            createEmployee();
-            break;
-            case "Go Back":
-            console.log("startPage -> case:", data.action);
-            startPage();
-            break;
-        }
+//Refactored my code - what I was doing wasn't working - was too complex.
+function startPage() {
+    inquirer.prompt(homeMenu).then((response) => {
+      switch (response.action) {
+        case "Add Employee":
+        createEmployee();
+          break;
+        case "Add Role":
+          createRole();
+          break;
+        case "Add Department":
+          createDepartment();
+          break;
+        case "View All Employees":
+          showEmployees();
+          break;
+        /*case "View All Employees By Role":
+          viewByRole();
+          break;*/
+        /*case "View All Employees By Department":
+          viewByDepartment();
+          break;*/
+        case "View All Roles":
+          showRoles();
+          break;
+        case "View All Departments":
+          showDepartments();
+          break;
+        /*case "Update An Employee Role":
+          updateEmployee();
+          break;*/
+        case "Exit":
+          connection.end();
+          break;
+        default:
+          connection.end();
+      }
     });
-};
+    // update arrays each time the init function is called
+    getDepartmentList();
+    getRoleList();
+    getEmployeeList;
+  }
 
-const departmentActionsMenu = () =>{
-    return inquirer
-    .prompt([{
-        type:"list",
-        name:"action",
-        message:"What would you like to do?",
-        choices:["Show All","Find","Add New","Go Back"], 
-    }])
-    .then((data)=> {
-        switch (data.action) {
-            case "Show All":
-            console.log("startPage -> case:", data.action);
-            showDepartments();
-            break;
-            case "Find":
-            console.log("startPage -> case:", data.action);
-            //showRoles();
-            break;
-            case "Add New":
-            console.log("startPage -> case:", data.action);
-            createDepartment();
-            break;
-            case "Go Back":
-            console.log("startPage -> case:", data.action);
-            startPage();
-            break;
-        }
-    });
-};
-const roleActionsMenu = () =>{
-    return inquirer
-    .prompt([{
-        type:"list",
-        name:"action",
-        message:"What would you like to do?",
-        choices:["Show All","Find","Add New","Go Back"], 
-    }])
-    .then((data)=> {
-        switch (data.action) {
-            case "Show All":
-            console.log("startPage -> case:", data.action);
-            showRoles();
-            break;
-            case "Find":
-            console.log("startPage -> case:", data.action);
-            //showRoles();
-            break;
-            case "Add New":
-            console.log("startPage -> case:", data.action);
-            createRole();
-            break;
-            case "Go Back":
-            console.log("startPage -> case:", data.action);
-            startPage();
-            break;
-        }
-    });
-};
 
-const showEmployees=()=>{
+
+//EMPLOYEES
+function getEmployeeList() {
+    connection.query(`SELECT last_name FROM employee`, function (err,res) {
+      if (err) throw err;
+      employeeArray = [];
+      for (i = 0; i < res.length; i++) {
+        employeeArray.push(res[i].last_name);
+      }
+    });
+  }
+
+  const showEmployees=()=>{
     var query = connection.query(
         "SELECT * FROM employee limit 25",
         function(err,res){
             if(err) throw err ;
-            x = (res)
-            for (i = 0 ; i < res.length ; i++){
-                console.log(`${res[i].id} - ${res[i].last_name}, ${res[i].first_name}`)
-            }
-            employeeActionsMenu();
-        });
-};
-
-const showDepartments=()=>{
-    var query = connection.query(
-        "SELECT * FROM department",
-        function(err,res){
-            if(err) throw err ;
-            x = (res)
-            for (i = 0 ; i < res.length ; i++){
-                console.log(`${res[i].id} - ${res[i].name}`)
-            }
+            console.table(res);
             startPage();
         });
 };
-
-const showRoles = () => {
-    var query = connection.query(
-        "SELECT * FROM role",
-        function (err, res) {
-            if (err) throw err;
-            x = (res)
-            for (i = 0 ; i < res.length ; i++){
-                console.log(`${res[i].id} - ${res[i].name}`)
-            }
-            startPage();
-        });
-};
-
-const selectEmployee=(id) =>{};
-const getDepartment=(id)=>{};
-const getRole=(id)=>{};
 
 const createEmployee=()=>{
- return inquirer
+    return inquirer
     .prompt([{
         type: "input",
         name: "first_name",
@@ -192,7 +124,15 @@ const createEmployee=()=>{
         type:"input",
         name:"last_name",
         message:"Last Name"
+    },
+    {
+        type:"list",
+        name:"role",
+        message:"Choose a Role",
+        choices: roleArray
     }
+
+
  ])
  .then ((data)=> {
         var query = "INSERT INTO employee (first_name,last_name) VALUES (?,?)";
@@ -200,6 +140,67 @@ const createEmployee=()=>{
         if (err) throw err;
         showEmployees()});
  });
+};
+
+
+
+//DEPARTMENTS
+  function getDepartmentList() {
+    connection.query(`SELECT name FROM department`, function (err,res) {
+      if (err) throw err;
+      deptArray = [];
+      for (i = 0; i < res.length; i++) {
+        deptArray.push(res[i].name);
+      }
+    });
+  }
+
+  const showDepartments=()=>{
+    var query = connection.query(
+        "SELECT * FROM department",
+        function(err,res){
+            if(err) throw err ;
+            console.table(res);
+            startPage();
+        });
+};
+
+const createDepartment=()=>{
+    return inquirer
+       .prompt([{
+           type: "input",
+           name: "name",
+           message: "Department Name:"
+       }
+    ])
+    .then ((data)=> {
+        var query = "INSERT INTO department (name) VALUES (?)";
+       connection.query(query, data.name , function(err, res) {
+        if (err) throw err;
+        showDepartments()});
+    });
+   };
+
+
+ //ROLES 
+  function getRoleList() {
+    connection.query(`SELECT name FROM role`, function (err,res) {
+      if (err) throw err;
+      roleArray = [];
+      for (i = 0; i < res.length; i++) {
+        roleArray.push(res[i].name);
+      };
+    });
+  }
+
+  const showRoles=() => {
+    var query = connection.query(
+        "SELECT * FROM role",
+        function (err, res) {
+            if (err) throw err;
+            console.table(res);
+            startPage();
+        });
 };
 
 const createRole=()=>{
@@ -222,20 +223,7 @@ const createRole=()=>{
     });
    };
 
-   const createDepartment=()=>{
-    return inquirer
-       .prompt([{
-           type: "input",
-           name: "name",
-           message: "Department Name:"
-       }
-    ])
-    .then ((data)=> {
-        var query = "INSERT INTO department (name) VALUES (?)";
-       connection.query(query, data.name , function(err, res) {
-        if (err) throw err;
-        showDepartments()});
-    });
-   };
-
-startPage();
+//UNUSED AT CURRENT MOMENT
+const selectEmployee=(id) =>{};
+const getDepartment=(id)=>{};
+const getRole=(id)=>{};
